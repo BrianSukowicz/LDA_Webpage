149e10c8bfe9715c098eee79f6fefaaef21b4fd6
Brian Sukowicz <briansukowicz@gmail.com>
2019-05-03 18:44:20 -0500

    fixed some errors

--- src/main/java/json/WebSecurityConfig.java
+++ src/main/java/json/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+        http.cors().and().csrf().disable();
     }
 
 
